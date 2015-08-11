b2f8e78d8671ed16c1538fc61aecdbbc9b9eb3e2
Adam Moelis <adam.moelis@gmail.com>
2015-08-11 15:33:46 -0400

    changes the vote_hash structure

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,40 +1,93 @@
-require_relative '../config/environment'
-
-class User
-
-
-
-
-end
 
 class Ranking_CLI
 
-  VALID_COMMANDS =["qb", "rb", "wr", "te", "k"]
+  attr_reader :player1_hash, :player2_hash, :vote_hash
+
+  VALID_COMMANDS ||= ["qb", "rb", "wr", "te", "k", "exit"]
+
+  
+  @@qb_vote_hash = {}
+  @@wr_vote_hash = {}
+  @@rb_vote_hash = {}
+  @@k_vote_hash = {}
+  @@te_vote_hash = {}
+
+  def initialize 
+    Get_Player_Data.new("QB").run.each do |player_hash|
+        @@qb_vote_hash[player_hash["displayName"]] = player_hash["votes"]
+    end
+
+    Get_Player_Data.new("WR").run.each do |player_hash|
+        @@wr_vote_hash[player_hash["displayName"]] = player_hash["votes"]
+    end
+
+    Get_Player_Data.new("RB").run.each do |player_hash|
+        @@rb_vote_hash[player_hash["displayName"]] = player_hash["votes"]
+    end
+
+    Get_Player_Data.new("K").run.each do |player_hash|
+        @@k_vote_hash[player_hash["displayName"]] = player_hash["votes"]
+    end
+
+    Get_Player_Data.new("TE").run.each do |player_hash|
+        @@te_vote_hash[player_hash["displayName"]] = player_hash["votes"]
+    end
+
+  end
+
+  def self.qb_vote_hash
+    @@qb_vote_hash
+  end
+
+  def self.wr_vote_hash
+    @@wr_vote_hash
+  end
+
+  def self.rb_vote_hash
+    @@rb_vote_hash
+  end
+
+  def self.te_vote_hash
+    @@te_vote_hash
+  end
+
+  def self.k_vote_hash
+    @@k_vote_hash
+  end
+
 
   def run
     initial_menu
-    menu
-    get_user_input
-    while invalid_input?
+    loop do
       menu
       get_user_input
+      
+      while invalid_input?
+        menu
+        get_user_input
+      end
+      player_generator(@user_input)
+      create_vote_hash
+      display_player_names
+      select_player
+      
     end
-    player_generator(@user_input)
-    display_player_names
+   
+
   end
 
   def initial_menu
     puts "Welcome to a Crowd-Sourced Fantasy Football Ranking System"
-    # user input for position, return position array
   end
 
   def menu
-    puts "What position would you like to choose (QB, RB, WR, TE, K)?"
+    puts "What position would you like to choose (QB, RB, WR, TE, K, exit to quit)?"
 
   end
 
 
   def player_generator(position)
+    @position = position
     @position_array = Get_Player_Data.new(position).run
     @random_player_array = @position_array.sample(2)
     @player1_hash = @random_player_array[0]
@@ -43,13 +96,16 @@ class Ranking_CLI
   end
 
   def display_player_names
-    puts "#{@player1_hash["displayName"]} vs. #{@player2_hash["displayName"]}"
+    puts "1.#{@player1_hash["displayName"]}\nvs.\n2.#{@player2_hash["displayName"]}"
+    puts "Choose 1 to select #{@player1_hash["displayName"]} or 2 to select #{@player2_hash["displayName"]}"
 
   end
   
 
   def get_user_input
     @user_input = gets.chomp.downcase
+    abort if @user_input == "exit"
+    @user_input
   end
 
   def invalid_input?
@@ -57,16 +113,25 @@ class Ranking_CLI
     !VALID_COMMANDS.include?(@user_input)
   end
 
+  def select_player
+    @user_input = get_user_input
+    @vote_hash[@player1_hash["displayName"]] += 1 if @user_input == "1"
+    @vote_hash[@player2_hash["displayName"]] += 1 if @user_input == "2"
+    
+  end
 
+  def create_vote_hash
 
+      @vote_hash = @position_array.each_with_object({}) do |player_hash, new_hash|
+        new_hash[player_hash["displayName"]] = player_hash["votes"]
+      end
+      
+  end
 
 
 
 
 end
-
-test = Ranking_CLI.new
-test.run
 
 
   
---
+++ lib/vote_output.rb
@@ -0,0 +1,13 @@
+# require_relative '../config/environment'
+
+# class Vote_Hash
+#   attr_accessor :vote_hash
+
+#   def initialize(position_array)
+#     vote_hash = position_array.each_with_object({}) {|player_hash, new_hash|new_hash[player_hash["displayName"]] = player_hash["votes"]}
+
+
+#   end
+
+
+# end
